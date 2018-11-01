1fef971e1e2eb5d0623d24d66f6c6c47bdf542ab
jscottsmith <jscsmith@gmail.com>
2018-11-01 14:39:07 -0700

    add accordion test for expanded state

--- _tests/molecules/Accordion.test.js
+++ _tests/molecules/Accordion.test.js
@@ -1,6 +1,7 @@
-/* globals mount */
+/* globals mount, shallow */
 import React from 'react';
 import Accordion from '../../components/molecules/Accordion';
+import AccordionItem from '../../components/atoms/AccordionItem';
 
 describe('Expect <Accordion>', () => {
     it('to render', () => {
@@ -11,11 +12,31 @@ describe('Expect <Accordion>', () => {
         };
         const wrapper = mount(
             <Accordion {...props}>
-                <li>Accordion Item 1</li>
-                <li>Accordion Item 2</li>
-                <li>Accordion Item 3</li>
+                <AccordionItem label="Accordion Item 1" />
+                <AccordionItem label="Accordion Item 2" />
+                <AccordionItem label="Accordion Item 3" />
             </Accordion>
         );
         expect(wrapper).toMatchSnapshot();
+    });
+
+    it('to render expanded', () => {
+        const props = {
+            isOpen: true
+        };
+        const wrapper = shallow(
+            <Accordion {...props}>
+                <AccordionItem label="Accordion Item 1" />
+                <AccordionItem label="Accordion Item 2" />
+                <AccordionItem label="Accordion Item 3" />
+            </Accordion>
+        );
+        wrapper.find('AccordionItem li').forEach(item => {
+            expect(item.hasClass('gds-accordion__item--active')).toBe(true);
+        });
+        wrapper.setProps({ isOpen: false });
+        wrapper.find('AccordionItem li').forEach(item => {
+            expect(item.hasClass('gds-accordion__item--active')).toBe(false);
+        });
     });
 });
